6efbf32496b4c6f9d5d35ac95a09169fd8d49c53
fabionas <fabionasario@gmail.com>
2019-09-24 22:43:15 -0300

    docs: add how to use env.sh on linux

--- env.sh
+++ env.sh
@@ -3,3 +3,9 @@
 _pwd=`pwd`
 export PATH="$PATH:$_pwd/node_modules/.bin"
 echo $PATH
+
+# How to run this file on linux from project root
+# $ source ./env.sh
+#
+# Test the local environment
+# $ gulp -v
